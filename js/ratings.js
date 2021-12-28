6115a442b5997f029f9a7a70bdba6ee294e0be3d
jreedmn <jane@red-pebble.com>
2021-12-28 14:37:57 -0600

    Finally!

--- js/ratings.js
+++ js/ratings.js
@@ -13,11 +13,15 @@ function collect_ratings() {
     ratings.count += parseInt(element.value);
     ratings.sum += rating * parseInt(element.value);
   });
-  if (count != 0) {
+
+  if (ratings.count != 0) {
     ratings.average = ratings.sum / ratings.count;
   }
+
   return ratings;
 }
 document.addEventListener("change", () => {
+  console.log("I am here now.");
   const ratings = collect_ratings();
+  document.querySelector("#average").value = ratings.average.toFixed(2);
 });
