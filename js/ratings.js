0148a199a9b0a62cdbd27d25cd33c6af3eaea4b5
jreedmn <jane@red-pebble.com>
2021-12-28 13:13:43 -0600

    Module 1, 1-6

---
+++ js/ratings.js
@@ -0,0 +1,19 @@
+function collect_ratings() {
+  let ratings = {
+    count: 0,
+    sum: 0,
+    average: 0,
+  };
+  let rating = 0;
+}
+const elements = document.querySelectorAll(".rating");
+
+// Pass an anonymous or arrow function to the forEach loop.
+// The function should accept a single argument called element.
+
+// In the forEach loop function, call the replace() function on the element.id.
+// To replace(), pass the correct arguments that replace the word star with nothing.
+// Pass this statement to parseInt() to convert the remaining string to a number.
+// All of this should be assigned to the existing rating variable.
+
+elements.forEach((element) => myfunction(element));
