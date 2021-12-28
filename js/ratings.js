ef967effeeebb7ce1dd5892ea52d0d20efad3b4a
jreedmn <jane@red-pebble.com>
2021-12-28 14:05:36 -0600

    Step 9

--- js/ratings.js
+++ js/ratings.js
@@ -11,4 +11,5 @@ const elements = document.querySelectorAll(".rating");
 elements.forEach((element) => {
   rating = parseInt(element.id.replace("star", ""));
   ratings.count += parseInt(element.value);
+  ratings.sum += rating * parseInt(element.value);
 });
